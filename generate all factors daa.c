62dd2c830614c334dc02f0397dd98ec49f5f7361
kjkamesh <149747963+kjkamesh@users.noreply.github.com>
2024-09-13 12:17:43 +0530

    Create generate all factors daa.c

---
+++ generate all factors daa.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+
+// Recursive function to find all factors of n
+
+void findFactors(int n, int i) {
+
+    if (i > n) 
+
+        return;
+
+    
+
+    if (n % i == 0)
+
+        printf("%d ", i);
+
+    
+
+    findFactors(n, i + 1);
+
+}
+
+
+
+int main() {
+
+    int n = 28; // Example value
+
+    printf("Factors of %d are: ", n);
+
+    findFactors(n, 1);
+
+    return 0;
+
+}
+
