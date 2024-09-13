1b8d9a7013a4ff7c174b319d1dc8616ac5f1bd78
kjkamesh <149747963+kjkamesh@users.noreply.github.com>
2024-09-13 12:10:06 +0530

    Create Copy One String to Another daa.c

---
+++ Copy One String to Another daa.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+
+void copyString(char *source, char *destination) {
+
+    if (*source == '\0') {
+
+        *destination = '\0';
+
+        return;
+
+    }
+
+    *destination = *source;
+
+    copyString(source + 1, destination + 1);
+
+}
+
+
+
+int main() {
+
+    char source[] = "Hello World"; // Change the string if needed
+
+    char destination[50];
+
+    
+
+    copyString(source, destination);
+
+    printf("Copied String: %s\n", destination);
+
+    return 0;
+
+}
+
